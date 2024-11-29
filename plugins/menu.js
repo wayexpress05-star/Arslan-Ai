287ee0c8cec76a97953d2625b748c042b6674890
Arslan-MD <numberkliye@gmail.com>
2024-11-30 00:03:06 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -40,7 +40,7 @@ let dec = `
 
  > © CREATED BY *ARSLANMD OFFICIAL*
 `
-await conn.sendMessage(from,{image:{url: `https://files.catbox.moe/n5vvij.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://files.catbox.moe/k64961.jpg`},caption:dec},{quoted:mek});
 
 }catch(e){
 console.log(e)
