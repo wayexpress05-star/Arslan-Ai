5aa5fcddff2e91dd960031d29838b46388d8de36
Arslan-MD <numberkliye@gmail.com>
2024-11-29 23:49:32 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "QWswSAAL#fsFP3lG5VRNC1V73gIG5Aj4zmF5qY2_Pti_I8pU3Khk",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/n5vvij.jpg",
-SUDO_NB: process.env.SUDO_NB || "263780934873",
+SUDO_NB: process.env.SUDO_NB || "923237045919",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
