b489369dae0faa6364fbceaf170347ecf31d5448
ArslanMD Official <numberkliye@gmail.com>
2024-12-09 12:35:21 +0500

    Update autobio.js

--- plugins/autobio.js
+++ plugins/autobio.js
@@ -1,40 +1,32 @@
-import { bot } from '../lib/plugins.js';
-import { autobioDBService, placeholderService } from '../sql/autobio.js';
+let handler = m => m
+handler.all = async function (m) {
+  let setting = global.db.data.settings[this.user.jid]
 
-let autobioInterval;
+  let bot = global.db.data.settings[this.user.jid] || {}
+  if (bot.autoBio) {
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime =
+        (await new Promise(resolve => {
+          process.once('message', resolve)
+          setTimeout(resolve, 1000)
+        })) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let bio = `\n🟢 Time Active ${muptime}\n\n ┃ 💎  By ArslanMD`
+    await this.updateProfileStatus(bio).catch(_ => _)
+    setting.status = new Date() * 1
+  }
+}
+export default handler
 
-bot(
-	{
-		pattern: 'autobio ?(.*)',
-		isPublic: false,
-		desc: 'Manage auto bio. Use "on" or "off".',
-		type: 'whatsapp',
-	},
-	async (message, match) => {
-		const action = match?.toLowerCase().trim();
-		const autobioConfig = await autobioDBService.getConfig();
-		const updateBio = async () => {
-			const updatedBio = await placeholderService.replacePlaceholders(autobioConfig.autobio_msg, message.client);
-			await message.client.updateProfileStatus(updatedBio);
-			console.log(`[Autobio] Bio updated: ${updatedBio}`);
-		};
-		if (action === 'on') {
-			if (autobioConfig.is_active) return message.send('_Autobio is already active!_');
-			await autobioDBService.setActiveStatus(true);
-			await updateBio();
-			autobioInterval = setInterval(async () => {
-				const currentConfig = await autobioDBService.getConfig();
-				if (!currentConfig.is_active) return clearInterval(autobioInterval);
-				await updateBio();
-			}, 60 * 1000);
-			return message.send('_Autobio activated! Bio will update every minute._');
-		}
-		if (action === 'off') {
-			if (!autobioConfig.is_active) return message.send('_Autobio is not active!_');
-			await autobioDBService.setActiveStatus(false);
-			clearInterval(autobioInterval);
-			return message.send('_Autobio deactivated!_');
-		}
-		return message.send('_Use "autobio on" or "autobio off"_');
-	},
-);
+function clockString(ms) {
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, ' Day(s) ️', h, ' Hour(s) ', m, ' Minute(s)']
+    .map(v => v.toString().padStart(2, 0))
+    .join('')
+	    }
