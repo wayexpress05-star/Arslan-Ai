6c9d86d89d3f66cd1ccb9381a47586370fd35088
ArslanMD Official <numberkliye@gmail.com>
2025-06-04 23:26:22 +0500

    Add files via upload

---
+++ whatsapp/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  sessionId: process.env.SESSION_ID || 'Paste_Your_Session_ID_Here',
+  prefix: '.',
+  botName: 'Arslan-MD',
+  ownerName: 'Arslan',
+  ownerNumber: ['923001234567'],
+  lang: 'EN',
+  autoRead: true,
+  autoBlock: true,
+  antiSpam: true
+};
