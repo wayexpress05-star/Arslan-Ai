86cf9315be1f54c2c9b8f887845f55f6ad451bd8
ArslanMD Official <numberkliye@gmail.com>
2025-06-16 18:46:47 +0500

    Update facebook.js

--- commands/facebook.js
+++ commands/facebook.js
@@ -84,7 +84,7 @@ async function facebookCommand(sock, chatId, message) {
         await sock.sendMessage(chatId, {
             video: { url: tempFile },
             mimetype: "video/mp4",
-            caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 𝗞𝗡𝗜𝗚𝗛𝗧-𝗕𝗢𝗧"
+            caption: "𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍𝚎𝚍 𝚋𝚢 𝙰𝚛𝚜𝚕𝚊𝚗-𝙼𝙳"
         }, { quoted: message });
 
         // Clean up temp file
