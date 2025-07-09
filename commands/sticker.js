b5d52f2aea8d33ab82edb0b37fd5769df36578cb
ArslanMD Official <numberkliye@gmail.com>
2025-07-10 02:43:15 +0500

    Update sticker.js

--- commands/sticker.js
+++ commands/sticker.js
@@ -36,8 +36,8 @@ async function stickerCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363348739987203@newsletter',
+                    newsletterName: 'Arslan-Ai',
                     serverMessageId: -1
                 }
             }
@@ -58,8 +58,8 @@ async function stickerCommand(sock, chatId, message) {
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'KnightBot MD',
+                        newsletterJid: '120363348739987203@newsletter',
+                        newsletterName: 'Arslan-Ai',
                         serverMessageId: -1
                     }
                 }
@@ -146,8 +146,8 @@ async function stickerCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363348739987203@newsletter',
+                    newsletterName: 'Arslan-Ai',
                     serverMessageId: -1
                 }
             }
