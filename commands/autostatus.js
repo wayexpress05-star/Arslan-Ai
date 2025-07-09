38fffddbbd4c4f9ad9206eb006b4c94ea3ce2473
ArslanMD Official <numberkliye@gmail.com>
2025-07-10 02:10:42 +0500

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -6,8 +6,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363348739987203@newsletter',
+            newsletterName: 'Arslan-Ai',
             serverMessageId: -1
         }
     }
