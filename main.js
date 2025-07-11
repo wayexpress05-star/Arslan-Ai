6c7a4eba99aa487df4b959167c19c15274a995f3
ArslanMD Official <numberkliye@gmail.com>
2025-07-11 14:36:01 +0500

    Update main.js

--- main.js
+++ main.js
@@ -637,7 +637,7 @@ async function handleMessages(sock, messageUpdate, printLog) {
                 await stickerTelegramCommand(sock, chatId, message);
                 break;
 
-            case userMessage === '.vv':
+            case userMessage === '.🥰, .vv, .❤️':
                 await viewOnceCommand(sock, chatId, message);
                 break;
             case userMessage === '.clearsession' || userMessage === '.clearsesi':
